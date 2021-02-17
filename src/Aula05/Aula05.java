939448f283582f28ac58439cd58d8144e0064410
Jeferson Siqueira <jeferson0306@msn.com>
2021-02-17 08:29:58 -0300

    update

--- src/Aula05/Aula05.java
+++ src/Aula05/Aula05.java
@@ -4,6 +4,24 @@ public class Aula05 {
 
 	public static void main(String[] args) {
 	
+		ContaBanco p1 = new ContaBanco();
+		p1.setNumConta(430951);
+		p1.setDono("Jeferson");
+		p1.estadoAtual();
+		p1.abrirConta("cc");
+		p1.depositar(100);
+		p1.fecharConta();
+		
+		
+		ContaBanco p2 = new ContaBanco();
+		p2.setNumConta(10049);
+		p2.setDono("Siqueira");
+		p2.estadoAtual();
+		p2.abrirConta("cp");
+		p2.depositar(500);
+		p2.sacar(100);
+		
+		
 
 	}
 
